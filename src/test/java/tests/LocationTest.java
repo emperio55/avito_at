ef5f35cc52c93d18030706229a16eebf233a7883
Aleksandr Shvidkii <alexshved55@gmail.com>
2025-11-04 19:18:00 +0300

    added Params for job

--- src/test/java/tests/LocationTest.java
+++ src/test/java/tests/LocationTest.java
@@ -7,12 +7,16 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("Выбор города")
 public class LocationTest extends TestBase {
+
         MainSteps steps = new MainSteps();
 
     @Test
         @DisplayName("Изменение города")
         @Tag("regres")
+        @Tag("browser")
         void changeCityTest() throws InterruptedException {
+        String browser = System.getProperty("browser", "chrome");
+        System.out.println(browser);
             steps.openMainPage();
             steps.removeCookiesBar();
             steps.clickRegion();
